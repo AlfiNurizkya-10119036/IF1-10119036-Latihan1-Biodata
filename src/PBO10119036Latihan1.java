b6072bea192e361fa846b13c005c60f1b30d20b3
ALFI22-01 <72850530+ALFI22-01@users.noreply.github.com>
2020-10-15 09:32:12 +0700

    Upload Project

---
+++ src/PBO10119036Latihan1.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Alfi Nurizkya
+ * NAMA     : ALFI NURIZKYA
+ * KELAS    : PBO
+ * NIM      : 10119036
+ * DESKRIPSI PROGRAM : PERINTAH MENAMPILKAN VALUE KE LAYAR
+ */
+public class PBO10119036Latihan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("BIODATA ROCKSTAR" + "/n" + "-----------------");
+        System.out.println("NAMA\t\t: ALFI NURIZKYA");
+        System.out.println("ALAMAT\t\t: Jalan Golf Dalam No. 45/G1");
+        System.out.println("TEMPAT LAHIR\t: BANDUNG");
+        System.out.println("TANGGAL LAHIR\t: 22 September 2001");
+        System.out.println("----------------------------------");
+        System.out.println("HOBI\t\t: Membaca");
+        System.out.println("MAKES\t\t: Mie Ayam");
+        System.out.println("MIKES\t\t: Teh Manis");
+    }
+    
+}
